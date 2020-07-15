48125763d667ee3aca33f37e6a4ab6fe7d35388c
souravsingh.blr <souravsingh.blr@gmail.com>
2020-07-15 20:57:13 +0530

    Project and Git Setup

---
+++ CheckoutSolution/CheckoutSolutionTest/CheckoutEngineTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CheckoutSolutionTest
+{
+    [TestClass]
+    public class CheckoutEngineTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
